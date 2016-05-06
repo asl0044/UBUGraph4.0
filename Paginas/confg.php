37a8afed965d8a3ea92f63381581d16d634eb318
asl0044 <asl0044@alu.ubu.es>
2016-05-06 11:38:20 +0200

    Versión inicial 3.0
    UBUGraph 3.0

---
+++ Paginas/confg.php
@@ -0,0 +1,14 @@
+<?php 
+	/**
+	 * Array que contiene los datos de configuracion necesarios para que UBUGrap funcione correctamente
+	 */
+	 $cfg = array(
+		"URL" => "www.ubugraph.es", //Contiene la URL de la pagina de la aplicacion.
+		"WIKI" => "http://www.wiki.ubugraph.duclos.es/", //Contiene la URL de la wiki
+		
+		"smtp" => "ssl://smtp.gmail.com", //SMTP de salida
+		"smtpPort" => 465, //Puerto del smtp
+		"mailUser" => "UBUGraph@gmail.com", //Usuario del correo que vamos a usar
+		"mailPass" => "UBUGraph2015" //Contraseña del correo
+		);
+?>
